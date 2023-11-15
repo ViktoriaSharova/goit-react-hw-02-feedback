84936c4b0ee1ed5961ff9a25c2e824baf3e5e307
ViktoriaSharova <131592560+ViktoriaSharova@users.noreply.github.com>
2023-11-15 19:18:06 +0100

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,60 +5,6 @@ import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Notification } from "./Notification/Notification";
 import 'normalize.css';
 
-// export class App extends Component {
-
-//   state = {
-//   good: 0,
-//   neutral: 0,
-//   bad: 0
-//   };
-
-//   countTotalFeedback = () => {
-//     return this.state.good + this.state.neutral + this.state.bad;
-//   };
-
-//   countPositiveFeedbackPercentage = () => {
-//     return Math.round(
-//       (this.state.good /
-//         (this.state.good + this.state.neutral + this.state.bad)) *
-//         100
-//     );
-//   };
-
-//     handleFeedback = (name) => {
-//     this.setState(prevState => {
-//       return {
-//         [name]: prevState[name] + 1
-//       };
-//     });
-//   };
-
-//   render() {
-//     const totalFeedback = this.countTotalFeedback();
-//   return (
-//     <div>
-//       <Section title="Please leave feed back">
-//           <FeedbackOptions
-//             options={Object.keys(this.state)}
-//             onLeaveFeedback={this.handleFeedback}
-//           />
-//       </Section>
-//       {!totalFeedback && <Notification />}
-//       {totalFeedback > 0 && (
-//         <Section title="Statistics">
-//           <Statistics
-//             good={this.state.good}
-//             neutral={this.state.neutral}
-//             bad={this.state.bad}
-//             total={this.countTotalFeedback()}
-//             positivePercentage={this.countPositiveFeedbackPercentage()}
-//           />
-//         </Section>
-//       )}
-//     </div>
-//   );
-//   };
-// }
 
 export class App extends Component {
   state = {
